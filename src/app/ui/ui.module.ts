47e3bd315dc375c1475f0afe47afdca941a708ba
Valdir <valdir@substractum.com.br>
2019-04-17 10:59:18 -0300

    Adicionar componente de layout

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -8,6 +8,7 @@ import { FooterComponent } from './footer/footer.component';
   declarations: [LayoutComponent, HeaderComponent, FooterComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [LayoutComponent]
 })
 export class UiModule { }
