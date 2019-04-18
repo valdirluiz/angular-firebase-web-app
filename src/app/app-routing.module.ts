dbe0abe609ca37f28dfe2b7f56d1c49f082aa4d7
Valdir <valdir@substractum.com.br>
2019-04-18 11:02:23 -0300

    Configurar rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
-const routes: Routes = [];
+import { AddHospedeComponent } from './hospedes/add-hospede/add-hospede.component';
+import { EditHospedeComponent  } from './hospedes/edit-hospede/edit-hospede.component';
+import { HospedeListComponent } from './hospedes/hospede-list/hospede-list.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/registrar-hospede', pathMatch: 'full' },
+  { path: 'registrar-hospede', component: AddHospedeComponent },
+  { path: 'visualizar-hospede', component: HospedeListComponent },
+  { path: 'editar-hospede/:id', component: EditHospedeComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [CommonModule,RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  declarations: []
 })
 export class AppRoutingModule { }
