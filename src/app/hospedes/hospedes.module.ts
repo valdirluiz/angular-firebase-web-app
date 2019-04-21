fa248431c9e7d5181b3b52a8818cabff98823708
Valdir Luiz <arnholdvaldir@gmail.com>
2019-04-21 19:56:28 -0300

    Adicionar footer

--- src/app/hospedes/hospedes.module.ts
+++ src/app/hospedes/hospedes.module.ts
@@ -1,19 +1,25 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AddHospedeComponent } from './add-hospede/add-hospede.component';
-import { EditHospedeComponent } from './edit-hospede/edit-hospede.component';
-import { HospedeListComponent } from './hospede-list/hospede-list.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { AddHospedeComponent } from "./add-hospede/add-hospede.component";
+import { EditHospedeComponent } from "./edit-hospede/edit-hospede.component";
+import { HospedeListComponent } from "./hospede-list/hospede-list.component";
+import { AppRoutingModule } from "../app-routing.module";
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgxPaginationModule } from 'ngx-pagination';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { NgxPaginationModule } from "ngx-pagination";
 
- 
 @NgModule({
-  declarations: [AddHospedeComponent, EditHospedeComponent, HospedeListComponent],
+  declarations: [
+    AddHospedeComponent,
+    EditHospedeComponent,
+    HospedeListComponent
+  ],
   imports: [
     CommonModule,
     FormsModule,
+    AppRoutingModule,
     ReactiveFormsModule,
-    NgxPaginationModule  ]
+    NgxPaginationModule
+  ]
 })
-export class HospedesModule { }
+export class HospedesModule {}
