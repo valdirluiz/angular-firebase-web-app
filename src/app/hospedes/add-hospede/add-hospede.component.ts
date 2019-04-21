9152c01f66e7c2767060e4950b2f78f7fbcccf72
Valdir Luiz <arnholdvaldir@gmail.com>
2019-04-21 20:49:30 -0300

    Adicionar campo de email e celular

--- src/app/hospedes/add-hospede/add-hospede.component.ts
+++ src/app/hospedes/add-hospede/add-hospede.component.ts
@@ -1,54 +1,68 @@
-import { Component, OnInit } from '@angular/core';
-import { CrudService } from '../../service/crud.service';    // CRUD services API
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'; // Reactive form services
-import { ToastrService } from 'ngx-toastr'; // Alert message using NGX toastr
+import { Component, OnInit } from "@angular/core";
+import { CrudService } from "../../service/crud.service"; // CRUD services API
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators
+} from "@angular/forms"; // Reactive form services
+import { ToastrService } from "ngx-toastr"; // Alert message using NGX toastr
 
 @Component({
-  selector: 'app-add-hospede',
-  templateUrl: './add-hospede.component.html',
-  styleUrls: ['./add-hospede.component.css']
+  selector: "app-add-hospede",
+  templateUrl: "./add-hospede.component.html",
+  styleUrls: ["./add-hospede.component.css"]
 })
 export class AddHospedeComponent implements OnInit {
-
-  public hospedeForm: FormGroup;   
+  public hospedeForm: FormGroup;
 
   constructor(
-    public crudApi: CrudService,   
-    public fb: FormBuilder,       
-    public toastr: ToastrService  
-  ) { }
-
+    public crudApi: CrudService,
+    public fb: FormBuilder,
+    public toastr: ToastrService
+  ) {}
 
   ngOnInit() {
-
-    this.crudApi.buscaHospedes();   
-    this.createHospedeForm();        
+    this.crudApi.buscaHospedes();
+    this.createHospedeForm();
   }
 
   createHospedeForm() {
     this.hospedeForm = this.fb.group({
-      nome: ['', [Validators.required, Validators.minLength(2)]],
-      paisOrigem: ['', [Validators.required]],
-
+      nome: ["", [Validators.required, Validators.minLength(2)]],
+      paisOrigem: ["", [Validators.required]],
+      email: [
+        "",
+        [
+          Validators.required,
+          Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$")
+        ]
+      ],
+      celular: ["", [Validators.required, Validators.pattern("^[0-9]+$")]]
     });
   }
 
   get nome() {
-    return this.hospedeForm.get('nome');
+    return this.hospedeForm.get("nome");
   }
 
   get paisOrigem() {
-    return this.hospedeForm.get('paisOrigem');
+    return this.hospedeForm.get("paisOrigem");
   }
- 
 
-  
+  get celular() {
+    return this.hospedeForm.get("celular");
+  }
+
+  get email() {
+    return this.hospedeForm.get("email");
+  }
+
   save() {
-    this.crudApi.addHospede(this.hospedeForm.value);  
-    this.toastr.success(this.hospedeForm.controls['nome'].value + ' cadastrado com sucesso!');
+    this.crudApi.addHospede(this.hospedeForm.value);
+    this.toastr.success(
+      this.hospedeForm.controls["nome"].value + " cadastrado com sucesso!"
+    );
     this.hospedeForm.reset();
-   };
-
-
-
+  }
 }
